4c53d86914a39aa0f0139b6b07ffacf7aa274eae
utsaho <pipul.boddyo@gmail.com>
2025-09-22 09:58:44 +0600

    live

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,7 +1,7 @@
 const Footer = () => {
     return (
         <div className="text-center my-3">
-            Created by <a href="https://tailadmin.com/" target="_blank" className="text-blue-600">TailAdmin</a> ~ Distributed By <a href="https://themewagon.com" target="_blank" className="text-blue-600">ThemeWagon</a>
+            Designed by <a href="https://tailadmin.com/" target="_blank" className="text-blue-600">TailAdmin</a>. Distributed by <a href="https://themewagon.com" target="_blank" className="text-blue-600">ThemeWagon</a>
         </div>
     );
 };
